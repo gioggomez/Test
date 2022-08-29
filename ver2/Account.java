55c475b42d35beee75192b0298a6abe04d45363a
gioggomez <112361710+gioggomez@users.noreply.github.com>
2022-08-29 07:25:09 -0400

    3rd change to account.java

--- ver2/Account.java
+++ ver2/Account.java
@@ -5,9 +5,6 @@ public class Account {
 	private double balance;
 	private String name;
 	
-	public Account(double balance) {
-		this("Unknown", balance);
-	}
 	
 	public Account(String name, double balance) {
 		this.name = name;
